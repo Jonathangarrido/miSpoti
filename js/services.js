a6b0987129e1e1111b02edd843b8625924c631d5
jonathan <jonathan.garrido1988@gmail.com>
2017-07-17 10:38:33 -0400

    token

--- js/services.js
+++ js/services.js
@@ -39,7 +39,7 @@
   angular
     .module('app.services', ['ngResource'])
     .constant('Host', 'api.spotify.com')
-    .constant('Authorization', 'BQBnO9680atgNYX17_EBomdcyVWvq8L3px3fZB3s42T-HjvIu3PWkT7ci2uiCYD57YEgB69RVIUu3U3RiMrdUUd-OpPE0M-nN40kVQ8GkeCKgaVmv-WN6ASCninJOMJg1awstzKn_5hFXXodUNTEpnnu6RfhgPA0sph9TtbVg3KJsHUDntG9RTjXQaJoAMGWKOjeAzqsxGFfHkjtblAU')
+    .constant('Authorization', 'BQDONHk2oq8x-atFmqPO5f1wwq30_KhYvX9gLJLYOpIxwuzEeUedtNupjBNsDrdlgqMphhojv22UOLRexCMihBujzJevrAAhzx6SfDQocRjHypSYY2vbbXy2MKLbKYn6TSlfvPzxbBpMiYH0DwjZ3XQPLzocVUubvoa1EVpPSAQSkp1xNzELkINoQfTh4LAnBXOvfIbXhZGZLQo9N3GD')
     .factory('TopChile', TopChile)
     .factory('Search', Search);
 
